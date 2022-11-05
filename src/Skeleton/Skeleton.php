afcc68ffbc8bc3e4ea7a8007f4c1a7854118e86a
Santiago Laparra <santiago.laparra@promocionesfarma.com>
2022-11-05 11:49:02 +0100

    skeleton

---
+++ src/Skeleton/Skeleton.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Santi\Skeleton;
+
+final class Skeleton
+{
+    public function __construct()
+    {
+        //to do
+    }
+}
---
+++ tests/Skeleton/SkeletonTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Santi\Tests\Skeleton;
+
+use Santi\Skeleton\Skeleton;
+use PHPUnit\Framework\TestCase;
+
+class SkeletonTest extends TestCase
+{
+    public function test_construct()
+    {
+        $this->assertTrue(true);
+    }
+}
